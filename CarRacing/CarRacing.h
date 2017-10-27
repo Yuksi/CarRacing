92288c7676338cf2ff3cfa7ad8d637bef97bd7ca
Yuksi <sam_d@ukr.net>
2017-10-27 05:58:11 +0300

    changes & uml

--- CarRacing/CarRacing.h
+++ CarRacing/CarRacing.h
@@ -1,14 +1,2 @@
 #include "stdafx.h"
-#include "Car.h"
 
-class CarRacing{
-
-private:
-	static const int row = 20;
-	static const int column = 17;
-	static char boardSymbol;
-
-public:
-	static void a();
-	static void b();
-};
